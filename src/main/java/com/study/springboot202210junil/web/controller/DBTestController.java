89322001fe4640104a2f305da751d7deb4c19991
junghyeonkyeong <gusrud8457@naver.com>
2022-12-19 21:50:07 +0900

    12/19

--- src/main/java/com/study/springboot202210junil/web/controller/DBTestController.java
+++ src/main/java/com/study/springboot202210junil/web/controller/DBTestController.java
@@ -2,12 +2,16 @@ package com.study.springboot202210junil.web.controller;
 
 import com.study.springboot202210junil.service.UserService;
 import com.study.springboot202210junil.web.dto.UserDto;
+
+import java.net.URI;
+
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController  
 @RequestMapping("/api/db/test")
 public class DBTestController {
 
@@ -16,8 +20,10 @@ public class DBTestController {
 
     @PostMapping("/user")
     public ResponseEntity<?> createUser(@RequestBody UserDto userDto) {
-        userService.addUser(userDto);
-        return ResponseEntity.created(null).body(true);
+        System.out.println(userDto);
+        int userId = userService.addUser(userDto);
+ //     return new ResponseEntity<>("응답할 데이터", HttpStatus.CREATED);
+        return ResponseEntity.created(URI.create("/api/db/test/user/" + userId)).body(userDto); // 빌더 형식
     }
 
     @GetMapping("/user/{userId}")
